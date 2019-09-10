3952be99878a3db1edf4239bc1f648e9bcb682ba
Mark <37361451+Mahrq@users.noreply.github.com>
2019-09-10 12:46:50 +1000

    Back up

---
+++ Assets/Source Code/Behaviours/PlayerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : Actor
+{
+    private Vector3 movementVector;
+    private float hInput, vInput;
+
+
+    private void Update()
+    {
+        hInput = Input.GetAxisRaw("Horizontal");
+        vInput = Input.GetAxisRaw("Vertical");
+        movementVector = new Vector3(hInput, 0, vInput) * movementSpeed;
+        actorTransform.Translate(movementVector);
+        
+    }
+
+
+}
---
+++ Assets/Source Code/Common/Actor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Actor : MonoBehaviour
+{
+    [SerializeField]
+    protected int health = 1;
+    public int Health { get { return health; } /*private set { health = value; }*/ }
+    [SerializeField]
+    [Range(0.1f, 1f)]
+    protected float movementSpeed = 5f;
+    [SerializeField]
+    protected Transform actorTransform;
+    [SerializeField]
+    protected Rigidbody actorRigidbody;
+    protected GameObject actorGameObject;
+    public bool IsDead { get; private set; }
+
+    virtual protected void Start()
+    {
+        actorGameObject = this.gameObject;
+        actorTransform = this.GetComponent<Transform>();
+        actorRigidbody = this.GetComponent<Rigidbody>();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            IsDead = true;
+            Death(IsDead);
+        }
+    }
+
+    virtual protected void Death(bool deathStatus)
+    {
+        if (deathStatus)
+        {
+            actorGameObject.SetActive(false);
+        }        
+    }
+
+}
